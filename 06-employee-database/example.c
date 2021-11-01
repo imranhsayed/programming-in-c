18353b71ac403e5fbe9e028e9870cae82084916c
Imran <imranhsayed@gmail.com>
2021-11-02 00:12:54 +0530

    Add code

--- 06-employee-database/example.c
+++ 06-employee-database/example.c
@@ -3,15 +3,16 @@
 
 struct Employee {
 	char name[50];
-	int ID;
-	char DepartmentName[50];
-	float Salary;
-	char Position[50];
-	char DateJoining[50];
+	int id;
+	char departmentName[50];
+	float salary;
+	char position[50];
+	char dateOfJoining[50];
 };
 
 struct Employee employees[100];
 struct Employee employee;
+int totalEmployees = 0;
 
 void createANewRecord() {
 
@@ -20,19 +21,46 @@ void createANewRecord() {
 
 	printf( "\n Enter name of the employee: " );
 	fgets( employee.name, 50, stdin );
+
+	printf( "\n Enter employee ID: " );
+    scanf( "%d", &employee.id );
+
+	fflush( stdin );
+    printf( "\n Enter department name: " );
+    fgets( employee.departmentName, 50, stdin );
+
+    printf( "\n Enter employee's salary: " );
+    scanf( "%f", &employee.salary );
+
+    fflush( stdin );
+    printf( "\n Enter employee's position: " );
+    fgets( employee.position, 50, stdin );
+
+    printf( "\n Enter employee's date of joining: " );
+    fgets( employee.dateOfJoining, 50, stdin );
+
+    employees[totalEmployees] = employee;
+    totalEmployees++;
+
+    printf( "\n A new employee is added. \n\n" );
+}
+
+void listRecords() {
+
 }
 
 int handleChoices( int choice ) {
 
 	if ( choice == 1 ) {
 		createANewRecord();
+	} else if ( choice == 2 ) {
+		listRecords();
 	}
 
 	return 0;
 }
 
 int main() {
-	int totalEmployees = 0;
 	int i;
 	int indexSelectedEmployee;
 	int userChoice = 0;
