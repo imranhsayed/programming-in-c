cfd4ef01853103649a9021ba4525ab3db57e7af0
Imran <imranhsayed@gmail.com>
2021-11-28 20:07:50 +0530

    Add numbers

--- 05-handling-files/example.c
+++ 05-handling-files/example.c
@@ -2,7 +2,7 @@
 
 int main() {
 	FILE *evenNumberFile, *oddNumberFile;
-	int countOfNumbers, givenNumbersArray[100], i;
+	int countOfNumbers, givenNumbersArray[100], i, num;
 
 	// Step One: Take the count of Numbers.
 	printf( "\n Enter the count of numbers: " );
@@ -14,8 +14,8 @@ int main() {
 	}
 
 	// Step Two: Open Two Writable Files called "even-numbers" and "odd-numbers"
-	evenNumberFile = fopen( "even-numbers", "w" );
-	oddNumberFile = fopen( "odd-numbers", "w" );
+	evenNumberFile = fopen( "even-numbers.txt", "w" );
+	oddNumberFile = fopen( "odd-numbers.txt", "w" );
 
 	// Step Three: Loop through the user fed numbers and put them conditionally into respective even and odd files.
 	for( i = 0; i < countOfNumbers; i++ ) {
@@ -31,6 +31,27 @@ int main() {
 	// Step Four: Close the Files.
 	fclose( evenNumberFile );
 	fclose( oddNumberFile );
+
+	// Step Five: Read from the file.
+	evenNumberFile = fopen( "even-numbers.txt", "r" );
+    oddNumberFile = fopen( "odd-numbers.txt", "r" );
+
+    printf("even-numbers.txt file content: ");
+
+	while( ( num = getw(evenNumberFile) ) !=EOF ) {
+		printf("%4d",num);
+		printf(",");
+	}
+
+	printf("\n odd-numbers.txt file content: ");
+
+	while( ( num = getw(oddNumberFile) ) != EOF ) {
+		printf("%4d",num);
+		printf(",");
+	}
+
+	fclose( evenNumberFile );
+    fclose( oddNumberFile );
 
 	return 0;
 }
