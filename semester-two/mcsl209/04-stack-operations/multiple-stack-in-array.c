0ccf4615e19e41d9be9fdb1d15b0c0f6290caa05
sum-lab <sum00ncat@gmail.com>
2022-09-11 17:45:44 +0530

    Multiple stacks: pop implementation

--- semester-two/mcsl209/04-stack-operations/multiple-stack-in-array.c
+++ semester-two/mcsl209/04-stack-operations/multiple-stack-in-array.c
@@ -4,7 +4,7 @@
 
 int array[SIZE];  // declaration of array type variable.  
 int top1 = -1;  
-int top2 = -1;
+int top2 = SIZE;
 int lengthOfStack1 = 5, lengthOfStack2 = 5;
 
 void push1(int item) {
@@ -28,23 +28,23 @@ void push2(int item) {
 int pop1() {
     int item;
     if (top1 == -1)
-        printf("Stack is empty");
+        printf("\n Stack is empty");
     else {
         item = array[top1];
         top1--;
-        printf("Item removed: %d", item);
+        printf("\n Item removed: %d", item);
     }
     return item;
 }
 
 int pop2() {
     int item;
-    if (top2 == -1)
-        printf("Stack is empty");
+    if (top2 == SIZE)
+        printf("\n Stack is empty");
     else {
         item = array[top2];
         top2++;
-        printf("Item removed: %d", item);
+        printf("\n Item removed: %d", item);
     }
     return item;
 }
@@ -66,5 +66,13 @@ int main() {
         printf("\n Stack Element %d: ", i);
         scanf("%d", &input);
         push2(input);
+    }
+
+    for (i = 0; i <= lengthOfStack1; i++) {
+        pop1();
+    }
+    
+    for (i = 0; i <= lengthOfStack2; i++) {
+        pop2();
     }
 }
